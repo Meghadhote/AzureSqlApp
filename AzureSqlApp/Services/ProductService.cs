fc058ddab9f4bd88a88c0c35f7e8c8ef0fe65bf2
AH834FV <AH834FV@IE3290886W1>
2022-09-15 13:35:49 +0100

    Add project files.

---
+++ AzureSqlApp/Services/ProductService.cs
@@ -0,0 +1,50 @@
+﻿using AzureSqlApp.Models;
+using System.Data.SqlClient;
+namespace AzureSqlApp.Services
+{
+    public class ProductService
+    {
+        private static string db_source = "meghaserver3000.database.windows.net";
+        private static string db_user = "sqladmin";
+        private static string db_pass = "Tushar@19871980";
+        private static string db_database = "meghadb";
+
+        private SqlConnection GetConnection()
+        {
+            var _builder = new SqlConnectionStringBuilder();
+            _builder.DataSource = db_source;
+            _builder.UserID = db_user;
+            _builder.Password = db_pass;
+            _builder.InitialCatalog = db_database;
+            return new SqlConnection(_builder.ConnectionString);
+
+        }
+
+        public List<Product> GetProducts()
+        {
+            SqlConnection conn = GetConnection();
+            List<Product> productslist = new List<Product>();
+            string statement = "select * from Products";
+            conn.Open();
+            SqlCommand cmd = new SqlCommand(statement, conn);
+            using(SqlDataReader reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    Product product=new Product()
+                    {
+                        ProductID=reader.GetInt32(0),
+                          ProductName=reader.GetString(1),
+                           Quantity=reader.GetInt32(2)
+
+                    };
+                    productslist.Add(product);
+                }
+            }
+            conn.Close();
+            return productslist;
+
+
+        }
+    }
+}
